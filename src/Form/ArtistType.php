f159b68039d5a5db357ca8a63c1bb8aba9b8801f
mayard689 <doc-albert@laposte.net>
2020-07-22 20:42:12 +0200

    crud done

---
+++ src/Form/ArtistType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Artist;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArtistType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('title')
+            ->add('story')
+            ->add('age')
+            ->add('height')
+            ->add('origin')
+            ->add('particularity')
+            ->add('incredible')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Artist::class,
+        ]);
+    }
+}
