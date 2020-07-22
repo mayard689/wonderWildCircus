8f07c17c0025254c74219844baaaf39580121c0b
mayard689 <doc-albert@laposte.net>
2020-07-22 19:00:13 +0200

    show cards css updated

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ShowRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="wild_circus_index")
      */
-    public function index() :Response
+    public function index(ShowRepository $showRepository) :Response
     {
+        $shows = $showRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'name' => 'Wonder Wild Circus',
+            'shows' => $shows,
         ]);
     }
 }
