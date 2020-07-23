00722b440b7ae71efc48eac4eebf86e75f8c06f0
mayard689 <doc-albert@laposte.net>
2020-07-23 08:36:19 +0200

    add artist on homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,12 +2,15 @@ import '../css/homepage.scss';
 
 window.addEventListener("scroll", function(){
 
-    let pageX=window.scrollY;
-    let nodes=document.getElementsByClassName('parallax-object');
-    for (let i=0; i<nodes.length; i++) {
-        let parallaxObject1= nodes[i];
-        let backgroundShift1=-0.05*(i+1)*pageX;
-        parallaxObject1.style.top=backgroundShift1+'px';
+    if( window.innerWidth > 500) {
+        let pageX=window.scrollY;
+        let nodes=document.getElementsByClassName('parallax-object');
+        for (let i=0; i<nodes.length; i++) {
+            let parallaxObject1= nodes[i];
+            let backgroundShift1=-0.05*(i+1)*pageX;
+            parallaxObject1.style.top=backgroundShift1+'px';
+        }
     }
+
 
 });
