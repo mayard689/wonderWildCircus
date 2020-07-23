042907f96bfb365e20254217b2a1db1d2a1aa1b4
mayard689 <doc-albert@laposte.net>
2020-07-23 03:41:14 +0200

    parallaxe done on story images

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,1 +1,13 @@
 import '../css/homepage.scss';
+
+window.addEventListener("scroll", function(){
+
+    let pageX=window.scrollY;
+    let nodes=document.getElementsByClassName('parallax-object');
+    for (let i=0; i<nodes.length; i++) {
+        let parallaxObject1= nodes[i];
+        let backgroundShift1=-0.05*(i+1)*pageX;
+        parallaxObject1.style.top=backgroundShift1+'px';
+    }
+
+});
