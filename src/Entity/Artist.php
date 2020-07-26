a308dcf70b784f25b9f89fcc9a3727008c87b6a4
mayard689 <doc-albert@laposte.net>
2020-07-27 00:34:03 +0200

    artist picture upload done

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use App\Repository\ArtistRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=ArtistRepository::class)
+ * @Vich\Uploadable
  */
 class Artist
 {
@@ -65,7 +68,7 @@ class Artist
      *
      * @Assert\NotBlank
      * @Assert\Type(
-     *     type="integer",
+     *     type="float",
      *     message="La valeur {{ value }} n'est pas de type {{ type }}."
      * )
      */
@@ -98,9 +101,68 @@ class Artist
     private $incredible;
 
     /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="artist_image", fileNameProperty="picture")
+     *
+     * @var File|null
+     */
+    private $pictureFile;
+
+    /**
      * @ORM\Column(type="string", length=255)
+     *
+     * @var string|null
      */
     private $picture;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTimeInterface|null
+     */
+    private $updatedAt;
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $updatedAt
+     */
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setPictureFile(?File $imageFile = null): void
+    {
+        $this->pictureFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getPictureFile(): ?File
+    {
+        return $this->pictureFile;
+    }
 
     public function getId(): ?int
     {
@@ -208,7 +270,7 @@ class Artist
         return $this->picture;
     }
 
-    public function setPicture(string $picture): self
+    public function setPicture(?string $picture): self
     {
         $this->picture = $picture;
 
